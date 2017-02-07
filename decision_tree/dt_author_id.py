6524d1c81b6a34737d41883095afdd75b99bcc6e
DavidMBP <steadypathapp@gmail.com>
2017-02-07 14:17:04 -0600

    finished 3 mini projects
    naive bayes
    svm
    descision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 
 ### features_train and features_test are the features for the training
@@ -19,7 +21,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
+# print len(features_train[0])
 
 
 #########################################################
@@ -27,5 +41,3 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-
-
--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -21,6 +26,32 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print features_train.shape
+
+gnb = GaussianNB()
+
+t0 = time()
+gnb.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+gpredict = gnb.predict(features_test)
+print "training time:", round(time()-t1, 3), "s"
+
+
+print accuracy_score(gpredict, labels_test)
+
+
+#
+# mnb = MultinomialNB()
+# mnb.fit(features_train, labels_train)
+# mpredict = mnb.predict(features_test)
+# print accuracy_score(mpredict, labels_test)
+#
+# bnb = BernoulliNB()
+# bnb.fit(features_train, labels_train)
+# bpredict = bnb.predict(features_test)
+# print accuracy_score(bpredict, labels_test)
 
 
 
@@ -29,5 +60,3 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-
-
