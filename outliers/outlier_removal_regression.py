cc2e708b721f7e15bc4304f842e6761834b8e278
DavidMBP <steadypathapp@gmail.com>
2017-02-08 23:24:41 -0600

    finished outliners miniproject

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import pickle
 
 from outlier_cleaner import outlierCleaner
+from sklearn.cross_validation import train_test_split
 
+from sklearn.linear_model import LinearRegression
 
 ### load up some practice data with outliers in it
 ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
@@ -20,20 +22,20 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-from sklearn.cross_validation import train_test_split
+
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
+# reg.predict(ages_test)
+# print reg.coef_
+# print reg.intercept_
+# print reg.score(feature_train, target_train)
+# print reg.score(ages_test, net_worths_test)
 
 
 try:
@@ -56,9 +58,6 @@ except NameError:
 
 
 
-
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -68,6 +67,10 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print reg.coef_
+        print reg.intercept_
+        print reg.score(ages_test, net_worths_test)
+
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
@@ -81,4 +84,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
