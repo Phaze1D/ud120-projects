32a743834847b2a2cf9172f7354217501296791f
DavidMBP <steadypathapp@gmail.com>
2017-02-09 21:01:30 -0600

    Finished text and features mini

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -2,13 +2,14 @@
 
 import pickle
 import numpy
+import pandas as pd
 numpy.random.seed(42)
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -27,6 +28,7 @@ vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
+print vectorizer.get_feature_names()[14343]
 
 
 ### a classic way to overfit is to use a small number
@@ -36,8 +38,15 @@ features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
 
-
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
 
+df = pd.DataFrame({'fm': clf.feature_importances_})
 
+print df[df.fm != 0]
