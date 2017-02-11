f007de4da37765e31a769078059d46d4e689a379
DavidMBP <steadypathapp@gmail.com>
2017-02-10 20:08:00 -0600

    Finished evaluation mini project

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,19 +13,37 @@
 
 import pickle
 import sys
+import pandas as pd
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.cross_validation import train_test_split
+
+
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
-### add more features to features_list!
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split( features, labels, test_size=0.3, random_state=42)
+
+# pd.DataFrame(f)
 
 
-### your code goes here 
+dt = DecisionTreeClassifier()
+dt.fit(features_train, labels_train)
+pred = dt.predict(features_test)
 
+print pred
+print labels_test
 
+print accuracy_score(pred, labels_test)
+print precision_score(pred, labels_test)
+print recall_score(pred, labels_test)
